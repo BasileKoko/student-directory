05d05c299e571dc956d4518954a07663f367e663
Basile Koko <basile_koko@hotmail.com>
2016-12-10 11:06:05 +0000

    refactoring script

--- script_toread_sourcecode.rb
+++ script_toread_sourcecode.rb
@@ -1,6 +1,5 @@
 def read_source_code
-  filename = $0
-   "The content of this method will be printed when it is executed!!"
-  File.readlines(filename)
+  "The content of this method will be printed when it is executed!!"
+  File.readlines($0)
 end
 puts read_source_code
