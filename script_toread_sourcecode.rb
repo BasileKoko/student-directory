7fe516477201d6d805d55cb0b1285c237abf5f19
Basile Koko <basile_koko@hotmail.com>
2016-12-10 11:11:47 +0000

    add comment to script

--- script_toread_sourcecode.rb
+++ script_toread_sourcecode.rb
@@ -1,5 +1,7 @@
 def read_source_code
   "The content of this method will be printed when it is executed!!"
+  # $0 is used to retrieve the file name script_toread_sourcecode.rb
+  # File.readlines() is the syntax for reading a file
   File.readlines($0)
 end
 puts read_source_code
